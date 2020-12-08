e028295ff6ef0e80f928a4c46a31d8cac23207f6
Kutsikas <mardus80@gmail.com>
2020-12-08 13:31:06 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 
 require("dotenv").config();
-const fs = require("fs");
-const time = require("./aeg.js");
+const message = require("./message");
 const invites = require("./invite");
 const status = require("./status");
 const client = new Discord.Client();
-const filenames = fs.readdirSync("./pildid");
+
 const Auditlog = require("discord-auditlog");
 
 Auditlog(client, {
@@ -23,44 +22,7 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
-  const { author, channel, content } = msg;
-  const authorKutsikas = "378303769835995156";
-  if (author.bot) return;
-  if (content === "?pilt") {
-    if (author.id === authorKutsikas) {
-      return handleSendSexyImage(msg);
-    } else {
-      return handleKutsikasOnlyMessage(msg);
-    }
-  }
-  if (content === "?uptime" && author.id === authorKutsikas) {
-    return botUpTime(msg);
-  }
-
-  if (
-    (author.id === authorKutsikas && content.includes("onju")) ||
-    (author.id === authorKutsikas && content.includes("eksju"))
-  ) {
-    return reactMessage(msg);
-  }
-  
+  message(msg);
 });
-
-const handleSendSexyImage = (msg) => {
-  const pildiNumber = Math.floor(Math.random() * filenames.length);
-  msg.channel.send({ files: ["./pildid/" + filenames[pildiNumber]] });
-};
-
-const handleKutsikasOnlyMessage = (msg) => {
-  msg.channel.send("Ma teenin ainult isand Kutsikat!");
-};
-
-const botUpTime = (msg) => {
-  msg.channel.send(time(client.uptime / 1000));
-};
-const reactMessage = (msg) => {
-  msg.react("👍");
-  msg.react("💋");
-};
 
 client.login(process.env.BOT_TOKEN);
---
+++ src/message.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const filenames = fs.readdirSync("./pildid");
+const time = require("./aeg.js");
+module.exports = function (msg) {
+  const { author, channel, content } = msg;
+  const authorKutsikas = "378303769835995156";
+  const handleSendSexyImage = (msg) => {
+    const pildiNumber = Math.floor(Math.random() * filenames.length);
+    msg.channel.send({ files: ["./pildid/" + filenames[pildiNumber]] });
+  };
+
+  const handleKutsikasOnlyMessage = (msg) => {
+    msg.channel.send("Ma teenin ainult isand Kutsikat!");
+  };
+
+  const botUpTime = (msg) => {
+    msg.channel.send(time(msg.client.uptime / 1000));
+  };
+  const reactMessage = (msg) => {
+    msg.react("👍");
+    msg.react("💋");
+  };
+  if (author.bot) return;
+  if (content === "?pilt") {
+    if (author.id === authorKutsikas) {
+      return handleSendSexyImage(msg);
+    } else {
+      return handleKutsikasOnlyMessage(msg);
+    }
+  }
+  if (content === "?uptime" && author.id === authorKutsikas) {
+    return botUpTime(msg);
+  }
+
+  if (
+    (author.id === authorKutsikas && content.includes("onju")) ||
+    (author.id === authorKutsikas && content.includes("eksju"))
+  ) {
+    return reactMessage(msg);
+  }
+};
